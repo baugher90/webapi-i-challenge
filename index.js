f63ed6a0a12c6a72807b3e5f407968eb477582c8
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-04-29 16:21:47 -0400

    yarn install, index file generic layout for all api requests

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // implement your API here
+const express = require('express');
+const server = express();
+
+
+server.post('api/users', (req, res) => {
+
+})
+
+server.get('api/users', (req, res) => {
+
+});
+
+server.get('api/users/:id', (req, res) => {
+
+});
+
+server.delete('api/users/:id', (req, res) => {
+
+});
+
+server.put('api/users/:id', (req, res) => {
+
+});
