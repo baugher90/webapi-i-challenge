d7d3b70384850ff1a5efe92ab48cd4f304bf4374
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-04-29 16:51:39 -0400

    wrote post but it's not wrking

--- index.js
+++ index.js
@@ -2,13 +2,26 @@
 const express = require('express');
 const server = express();
 server.listen(4000, () => console.log('API running on port 4000'));
+const db = require('./data/db.js');
 
 server.get('/', (req, res) => {
     res.send('server running')
 });
 
 server.post('api/users', (req, res) => {
-
+    const userInfo = req.body;
+    db.insert
+      .add(userInfo)
+      .then(users => {
+          if(find) {
+            res.status(201).json({success: true, users});
+          } else {
+            res.status(400).json({success: true, errorMessage: "Please provide name and bio for the user."});
+          }
+      })
+      .catch(err => {
+          res.status(err.code).json({success: false,  error: "There was an error while saving the user to the database"})
+      })
 })
 
 server.get('api/users', (req, res) => {
