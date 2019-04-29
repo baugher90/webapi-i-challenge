923c5eab055507a104a92c07b12e771153204d38
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-04-29 16:28:57 -0400

    added data base, server listen, and set up get to base level

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // implement your API here
 const express = require('express');
 const server = express();
+server.listen(4000, () => console.log('API running on port 4000'));
 
+server.get('/', (req, res) => {
+    res.send('server running')
+});
 
 server.post('api/users', (req, res) => {
 
